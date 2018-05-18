4687c2bf797ced96052797e43a2a5db74b49fec4
Thanh Nguyen <tatthanh.dev@gmail.com>
2018-05-18 15:48:18 +0700

    show toastr in admin-metronic

--- resources/views/pages/admin/metronic/layout/scripts.blade.php
+++ resources/views/pages/admin/metronic/layout/scripts.blade.php
@@ -1,8 +1,15 @@
 <script src="{!! \URLHelper::asset('metronic/vendors/base/vendors.bundle.js', 'admin') !!}"></script>
 <script src="{!! \URLHelper::asset('metronic/demo/default/base/scripts.bundle.js', 'admin') !!}"></script>
+<script src="{!! \URLHelper::asset('metronic/js/script.js', 'admin') !!}"></script>
 
 <script type="text/javascript">
     var Boilerplate = {
         'csrfToken': "{!! csrf_token() !!}"
     };
+
+    @if(Session::has('message-success'))
+        toastr.success("{{ Session::get('message-success') }}", "Success");
+    @elseif(Session::has('message-failed'))
+        toastr.error("{{ Session::get('message-failed') }}", "Error");
+    @endif
 </script>
